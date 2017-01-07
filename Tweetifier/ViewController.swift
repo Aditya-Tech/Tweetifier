7089f63542a9766df18cf1c8c036d5ebdc5f95aa
Aditya Paruchuri <adityasparuchuri@gmail.com>
2017-01-06 20:09:30 -0500

    Initial Commit

---
+++ Tweetifier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweetifier
+//
+//  Created by Aditya Paruchuri on 1/6/17.
+//  Copyright © 2017 Aditya Paruchuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
